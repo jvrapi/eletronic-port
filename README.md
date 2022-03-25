15495aa32598ac063bada2031f533f2c26a3057e
João Vitor Lima <joao.lima@devio.com.br>
2022-03-24 23:35:07 -0300

    Limit user to try three times; User cant try more than three times in sequency;

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 ## To do list:
 
-- [ x ] Add button to open the lock from inside
-- [  ] Limit user to try open the lock three times 
-- [  ] The user can't try more than three times in senquency  
+- [x] Add button to open the lock from inside
+- [x] Limit user to try open the lock three times 
+- [x] The user can't try more than three times in senquency  
--- control.c
+++ control.c
@@ -1,34 +1,10 @@
-/* ******************** Controle de acesso por senha Manual do Mundo ********************
-   Guia de conexão:
-    
-   Arduino:
-   LINHA 1 TECLADO: Pino Digital 11
-   LINHA 2 TECLADO: Pino Digital 10
-   LINHA 3 TECLADO: Pino Digital 9
-   LINHA 4 TECLADO: Pino Digital 8
-
-   COLUNA 1 TECLADO: Pino Digital 7
-   COLUNA 2 TECLADO: Pino Digital 6
-   COLUNA 3 TECLADO: Pino Digital 5
-   COLUNA 4 TECLADO: Pino Digital 4
-   
-   LED VERMELHO: Pino Digital 12
-   LED VERDE: Pino Digital 13
-   BUZZER: Pino Digital 2
-   MÓDULO RELÊ: Pino Digital 3 (na simulação do Tinkercad temos um led azul no lugar do módulo relê, quando ligamos o módulo não precisamos usar o resistor)
-
-   
-  
-   Este código utiliza a biblioteca Keypad
-      
- ***************************************************************************** */
-
-// Inclusão da biblioteca do módulo teclado
 #include <Keypad.h>
+
+
 
 /************************ DEFINIÇÃO DE SENHA ******************************* */
 
-char* senha = "ABC123";  // defina a senha aqui, ela deve conter seis dígitos
+char* senha = "ABC123"; // instancia a senha
 
 /************************ CONFIGURAÇÕES TECLADO ******************************* */
 
@@ -42,89 +18,144 @@ char digitos[LINHAS][COLUNAS] = {
 {'*','0','#','D'}}; // todos os caracteres do teclado
 
 byte pinosLinha[LINHAS] = { 11, 10, 9, 8 }; // pinos de conexão das linhas no arduino 
-byte pinosColuna[COLUNAS] = { 7, 6, 5, 4 }; // pinos de conexão das colunas no arduino 
+byte pinosColuna[COLUNAS] = { 7, 6, 5, 4 }; // pinos de conexão das colunas no arduino
+
+
 
 Keypad keypad = Keypad( makeKeymap(digitos), pinosLinha, pinosColuna, LINHAS, COLUNAS ); // objeto de leitura das teclas com os parâmetros lidos
 
 /************************ VARIÁVEIS AUXILIARES ******************************* */
 
 int position = 0;
-int botaoAbertura = 1;
+
+int botaoAbertura = 3;
 int ledVermelho = 12;
 int ledVerde = 13;
-int releFechadura = 3; //led azul substituir por módulo relé
+int releFechadura = 1; //led azul substituir por módulo relé
 int buzzer = 2;
-int tempoBuzz = 50;
 int tempoAberto = 2000;
+int errado = 0; // Conta quantas vezes tentou um acesso errado
+int registeringPassword = 0;
+
+String passwordRegistered = "";
+
 
 /***************************************************************************** */
 
-void setup()
-{ 
+void setup(){ 
   
-estadoPorta(true); // diz se a porta está ou não trancada
-
-pinMode(ledVermelho,OUTPUT); 
-pinMode(ledVerde,OUTPUT);
-pinMode(releFechadura,OUTPUT);
-pinMode(buzzer,OUTPUT); // define os pinos de saída do arduino
-pinMode(botaoAbertura, INPUT);
+  lockPort(); // diz se a porta está ou não trancada
+  pinMode(ledVermelho,OUTPUT); 
+  pinMode(ledVerde,OUTPUT);
+  pinMode(releFechadura,OUTPUT);
+  pinMode(buzzer,OUTPUT); // define os pinos de saída do arduino
+  pinMode(botaoAbertura, INPUT);
+  Serial.begin(9600);
 
 }
 
-void loop()
-{
+void loop() {
   
-  if(digitalRead(botaoAbertura) == 1){
-  estadoPorta(false);
+  int buttonState = digitalRead(botaoAbertura);
+  
+  if(buttonState == HIGH){
+    handlePortState();
     delay(100);
   }else{
+    
+  	checkPassword();
+  }
   
-char digito = keypad.getKey(); // faz a leitura das teclas
+}
 
-if (digito !=0)
-{
-  digitalWrite(buzzer, HIGH);
-  delay(tempoBuzz);
-  digitalWrite(buzzer, LOW); // acionamento do buzzer ao apertar qualquer tecla
 
-if (digito == senha[position]) // verifica se o dígito apertado corresponde ao equivalente da senha
-{
-position ++;
-}
-else { // retorna a leitura para a primeira posição de leitura da senha quando o dígito apertado não corresponde ao correto 
-position = 0;
-}
-if (position == 6) // altera estado da porta  
-{
-estadoPorta(false);
-}
-delay(100);
-}
+void checkPassword(){
+  char digit = keypad.getKey();
+
+  
+  if(digit != 0 && errado < 3) {
+    keyboardSound();
+    
+    if(digit == '*' && registeringPassword == 0){
+      registeringPassword = 1;
+    }else if(digit == '#' && registeringPassword == 1){
+     registeringPassword = 0;
+     verifyPasswordIsCorrect();
+    }else{
+    	passwordRegistered += digit;
+    }
+    
+    
   }
 }
 
-void estadoPorta(int trancado)
-{
-  
-if (trancado) // trancamento da porta
-{
-digitalWrite(ledVermelho, HIGH);
-digitalWrite(ledVerde, LOW);
-digitalWrite(releFechadura, LOW); // importante: para uso com rele de acionamento da fechadura LOW deve ser subsstituído por HIGH
+void verifyPasswordIsCorrect(){
+  if(passwordRegistered == senha){
+    handlePortState();
+  }else{
+    accessDenied();
+  }
+  passwordRegistered = "";
 }
 
-else // abertura da porta
-{
-digitalWrite(ledVermelho, LOW);
-digitalWrite(ledVerde, HIGH);
-digitalWrite(releFechadura, HIGH); // importante: para uso com rele de acionamento da fechadura HIGH deve ser subsstituído por LOW
-digitalWrite(buzzer, HIGH);
-delay(tempoBuzz*10);
-digitalWrite(buzzer, LOW); // buzzer indica o acerto da senha 
-delay(tempoAberto);
-position = 0;
-digitalWrite(releFechadura, HIGH);
-estadoPorta(true); // tranca a porta novamente 
+
+
+void keyboardSound(){
+  digitalWrite(buzzer, HIGH);
+  delay(50);
+  digitalWrite(buzzer, LOW);
 }
+
+
+void activateAlarm(){
+  digitalWrite(buzzer, HIGH);
+  delay (5000);
+  digitalWrite(buzzer, LOW);
+  errado = 0;
+
+}
+
+void accessDenied(){
+  errado++;
+  if(errado < 3){
+    digitalWrite(buzzer, HIGH);
+    delay(200);
+    digitalWrite(buzzer, LOW);
+  } else{
+    activateAlarm();
+  }
+
+}
+
+
+void handlePortState(){
+  accessGranted();
+  delay (1000);
+  lockPort();
 } 
+
+
+void accessGranted(){
+  acessGrantedSound();
+  digitalWrite(ledVermelho, LOW);
+  digitalWrite(ledVerde, HIGH);
+  digitalWrite(releFechadura, HIGH); // importante: para uso com rele de acionamento da fechadura HIGH deve ser subsstituído por LOW
+  delay(tempoAberto);
+  digitalWrite(releFechadura, HIGH);
+  
+}
+
+void acessGrantedSound(){
+  digitalWrite(buzzer, HIGH);
+  delay(100);
+  digitalWrite(buzzer, LOW);
+  
+}
+
+void lockPort(){
+  digitalWrite(ledVermelho, HIGH);
+  digitalWrite(ledVerde, LOW);
+  digitalWrite(releFechadura, LOW);// importante: para uso com rele de acionamento da fechadura LOW deve ser subsstituído por HIGH
+}
+
+  
